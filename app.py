7052bc04a0a24f859d2066ba0a849f5eab6b22ce
kevin <kevinklgvg@gmail.com>
2026-03-16 17:30:18 -0300

    fiirst commit

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ from database import Load_data
 def main():
     dataframe = Load_data(ID_DB="1sGNTms97VI-JV7EqSipsKOoRNIGVGIrhXjQWH010EBM", ID_SHEET="1339436993")
     st.title("dashboard")   
+    
     st.dataframe(dataframe) 
+
