7c551e2220fb4a201ff1e12236223597811f37c8
kevin <kevinklgvg@gmail.com>
2026-03-16 17:55:43 -0300

    indices

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ def main():
     dataframe = Load_data(ID_DB="1sGNTms97VI-JV7EqSipsKOoRNIGVGIrhXjQWH010EBM", ID_SHEET="1339436993")
     
     st.set_page_config(page_title="Dashboard de Carga", page_icon="📊", layout="wide")
-    st.title("dashboard")   
+    st.title("dashboard")
+       
+    
     
     # Indices
-    st.metric(label="Qtd coletas", value=str(len(dataframe[dataframe['Tipo'] == "Coleta"])))
+    col1, col2 = st.columns(2, gap="large")
     
-    st.metric(label="Qtd entregas", value=str(len(dataframe[dataframe['Tipo'] == "Entrega"])))
+    col1.metric(label="Qtd coletas", value=str(len(dataframe[dataframe['Tipo'] == "Coleta"])))
+    
+    col2.metric(label="Qtd entregas", value=str(len(dataframe[dataframe['Tipo'] == "Entrega"])))
     
     
     #frame
